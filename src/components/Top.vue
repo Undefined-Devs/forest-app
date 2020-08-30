cc0022d675d164dac5583530e29b19882202dfe9
Cristopher <cristopher.p.s.cto@gmail.com>
2020-08-30 06:34:45 -0500

    se agregan iconos

--- src/components/Top.vue
+++ src/components/Top.vue
@@ -2,27 +2,27 @@
   <div>
     <!-- Page Heading -->
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
-      <h1 class="h3 mb-0 text-gray-800">Top Mundial</h1>
+      
+      <h1 class="h3 mb-0 text-gray-800">
+        <i class="fas fa-crown mr-1"></i>
+        Top Mundial 
+      </h1>
     </div>
 
     <!-- Content Row -->
     <div class="row">
       <!-- Earnings (Monthly) Card Example -->
       <div class="col-xl-3 col-md-6 mb-4">
-        <div class="card border-left-primary shadow h-100 py-2">
+        <div class="card border-left-success shadow h-100 py-2">
           <div class="card-body">
             <div class="row no-gutters align-items-center">
               <div class="col mr-2">
                 <div class="h5 mb-0 text-gray-800">
                   <strong>Cristopher PS </strong>
-                  <img
-                    class="img-profile rounded-circle"
-                    src="https://source.unsplash.com/QAB-WJcbgJk/60x60"
-                    width="15%"
-                  />
+                  <i class="fas fa-crown"></i>
                 </div>
-                <div class="font-weight-bold text-primary text-uppercase mb-1">
-                  +40k
+                <div class="font-weight-bold text-success text-uppercase mb-1">
+                  +85k
                 </div>
               </div>
             </div>
@@ -37,15 +37,11 @@
             <div class="row no-gutters align-items-center">
               <div class="col mr-2">
                 <div class="h5 mb-0 text-gray-800">
-                  <strong>Cristopher PS </strong>
-                  <img
-                    class="img-profile rounded-circle"
-                    src="https://source.unsplash.com/QAB-WJcbgJk/60x60"
-                    width="15%"
-                  />
+                  <strong>Wiar 8 </strong>
+                  <i class="fas fa-trophy"></i>
                 </div>
                 <div class="font-weight-bold text-primary text-uppercase mb-1">
-                  +40k
+                  +60k
                 </div>
               </div>
             </div>
@@ -55,20 +51,16 @@
 
       <!-- Earnings (Monthly) Card Example -->
       <div class="col-xl-3 col-md-6 mb-4">
-        <div class="card border-left-primary shadow h-100 py-2">
+        <div class="card border-left-warning shadow h-100 py-2">
           <div class="card-body">
             <div class="row no-gutters align-items-center">
               <div class="col mr-2">
                 <div class="h5 mb-0 text-gray-800">
-                  <strong>Cristopher PS </strong>
-                  <img
-                    class="img-profile rounded-circle"
-                    src="https://source.unsplash.com/QAB-WJcbgJk/60x60"
-                    width="15%"
-                  />
+                  <strong>Vurokrazia </strong>
+                  <i class="fas fa-medal"></i>
                 </div>
-                <div class="font-weight-bold text-primary text-uppercase mb-1">
-                  +40k
+                <div class="font-weight-bold text-warning text-uppercase mb-1">
+                  +45k
                 </div>
               </div>
             </div>
@@ -78,19 +70,15 @@
 
       <!-- Earnings (Monthly) Card Example -->
       <div class="col-xl-3 col-md-6 mb-4">
-        <div class="card border-left-primary shadow h-100 py-2">
+        <div class="card border-left-danger shadow h-100 py-2">
           <div class="card-body">
             <div class="row no-gutters align-items-center">
               <div class="col mr-2">
                 <div class="h5 mb-0 text-gray-800">
-                  <strong>Cristopher PS </strong>
-                  <img
-                    class="img-profile rounded-circle"
-                    src="https://source.unsplash.com/QAB-WJcbgJk/60x60"
-                    width="15%"
-                  />
+                  <strong>iMGil </strong>
+                  <i class="fas fa-award"></i>
                 </div>
-                <div class="font-weight-bold text-primary text-uppercase mb-1">
+                <div class="font-weight-bold text-danger text-uppercase mb-1">
                   +40k
                 </div>
               </div>
