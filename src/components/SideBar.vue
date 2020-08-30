fdb73b25459eef495bb39d0286c2e1562a46ad5d
Cristopher <cristopher.p.s.cto@gmail.com>
2020-08-30 06:34:14 -0500

    se mejoran opciones en el sidebar

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -5,8 +5,8 @@
     id="accordionSidebar"
   >
     <!-- Sidebar - Brand -->
-    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-      <i class="fas fa-tree"></i>
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="/">
+      <i class="fas fa-tree" style="font-size:1.6rem;"></i>
       <div class="sidebar-brand-text mx-3">Forest App <sup>1.0</sup></div>
     </a>
 
@@ -15,14 +15,10 @@
 
     <li class="nav-item">
       <a
-        class="nav-link text-center"
-        href="#"
+        class="nav-link text-center text-white"
       >
-        <span class="mr-2 d-none d-lg-inline small"><strong>Cristopher PS</strong></span>
-        <img
-          class="img-profile rounded-circle"
-          src="https://source.unsplash.com/QAB-WJcbgJk/60x60"
-        />
+        <span class="mr-2 d-none d-lg-inline "><strong>Cristopher PS</strong></span>
+        <i class="fas fa-user text-white"></i>
       </a>
     </li>
 
@@ -34,28 +30,21 @@
       MENU
     </div>
 
-    <li class="nav-item active">
+    <li :class="route == 'Home' ? 'nav-item active' : 'nav-item'">
       <a class="nav-link" href="/">
         <i class="fas fa-home"></i>
         <span>Inicio</span></a
       >
     </li>
-
-    <li class="nav-item">
+    
+    <li :class="route == 'Challenges' ? 'nav-item active' : 'nav-item'">
       <a class="nav-link" href="/challenges">
         <i class="fas fa-fan"></i>
         <span>Retos</span></a
       >
     </li>
 
-    <li class="nav-item">
-      <a class="nav-link" href="/new-user">
-        <i class="fas fa-users"></i>
-        <span>Registrarse</span></a
-      >
-    </li>
-
-    <li class="nav-item">
+    <li :class="route == 'NewPost' ? 'nav-item active' : 'nav-item'">
       <a class="nav-link" href="/new-post">
         <span>Aceptar Reto</span></a
       >
@@ -64,6 +53,27 @@
     <!-- Divider -->
     <hr class="sidebar-divider d-none d-md-block" />
 
+    <li :class="route == 'Login' ? 'nav-item active' : 'nav-item'">
+      <a class="nav-link" href="/login">
+        <i class="fas fa-sign-in-alt"></i>
+        <span>Ingresar</span></a
+      >
+    </li>
+
+    <li :class="route == 'NewUser' ? 'nav-item active' : 'nav-item'">
+      <a class="nav-link" href="/new-user">
+        <i class="fas fa-users"></i>
+        <span>Registrarse</span></a
+      >
+    </li>
+
+    <li :class="'nav-item'">
+      <a class="nav-link" href="/">
+        <i class="fas fa-sign-out-alt"></i>
+        <span>Salir</span></a
+      >
+    </li>
+
   </ul>
   <!-- End of Sidebar -->
 </template>
@@ -71,6 +81,11 @@
 <script>
 export default {
   name: 'SideBar',
+  computed: {
+    route () {      
+      return this.$router.history.current.name;
+    }
+  }
 };
 </script>
 
