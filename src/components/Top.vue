e6710718ec096c1bce5ac6c77e60063acc5c7cd8
Cristopher <cristopher.p.s.cto@gmail.com>
2020-08-29 23:07:19 -0500

    se crea componente para tops

---
+++ src/components/Top.vue
@@ -0,0 +1,112 @@
+<template>
+  <div>
+    <!-- Page Heading -->
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+      <h1 class="h3 mb-0 text-gray-800">Top Mundial</h1>
+    </div>
+
+    <!-- Content Row -->
+    <div class="row">
+      <!-- Earnings (Monthly) Card Example -->
+      <div class="col-xl-3 col-md-6 mb-4">
+        <div class="card border-left-primary shadow h-100 py-2">
+          <div class="card-body">
+            <div class="row no-gutters align-items-center">
+              <div class="col mr-2">
+                <div class="h5 mb-0 text-gray-800">
+                  <strong>Cristopher PS </strong>
+                  <img
+                    class="img-profile rounded-circle"
+                    src="https://source.unsplash.com/QAB-WJcbgJk/60x60"
+                    width="15%"
+                  />
+                </div>
+                <div class="font-weight-bold text-primary text-uppercase mb-1">
+                  +40k
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- Earnings (Monthly) Card Example -->
+      <div class="col-xl-3 col-md-6 mb-4">
+        <div class="card border-left-primary shadow h-100 py-2">
+          <div class="card-body">
+            <div class="row no-gutters align-items-center">
+              <div class="col mr-2">
+                <div class="h5 mb-0 text-gray-800">
+                  <strong>Cristopher PS </strong>
+                  <img
+                    class="img-profile rounded-circle"
+                    src="https://source.unsplash.com/QAB-WJcbgJk/60x60"
+                    width="15%"
+                  />
+                </div>
+                <div class="font-weight-bold text-primary text-uppercase mb-1">
+                  +40k
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- Earnings (Monthly) Card Example -->
+      <div class="col-xl-3 col-md-6 mb-4">
+        <div class="card border-left-primary shadow h-100 py-2">
+          <div class="card-body">
+            <div class="row no-gutters align-items-center">
+              <div class="col mr-2">
+                <div class="h5 mb-0 text-gray-800">
+                  <strong>Cristopher PS </strong>
+                  <img
+                    class="img-profile rounded-circle"
+                    src="https://source.unsplash.com/QAB-WJcbgJk/60x60"
+                    width="15%"
+                  />
+                </div>
+                <div class="font-weight-bold text-primary text-uppercase mb-1">
+                  +40k
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- Earnings (Monthly) Card Example -->
+      <div class="col-xl-3 col-md-6 mb-4">
+        <div class="card border-left-primary shadow h-100 py-2">
+          <div class="card-body">
+            <div class="row no-gutters align-items-center">
+              <div class="col mr-2">
+                <div class="h5 mb-0 text-gray-800">
+                  <strong>Cristopher PS </strong>
+                  <img
+                    class="img-profile rounded-circle"
+                    src="https://source.unsplash.com/QAB-WJcbgJk/60x60"
+                    width="15%"
+                  />
+                </div>
+                <div class="font-weight-bold text-primary text-uppercase mb-1">
+                  +40k
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Top',
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss"></style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import Vue from 'vue';
 import App from './App.vue';
+import SideBar from './components/SideBar.vue';
+import Top from './components/Top.vue';
 import './registerServiceWorker';
 import router from './router';
 import store from './store';
 
 Vue.config.productionTip = false;
+
+Vue.component('sidebar', SideBar);
+Vue.component('top', Top);
 
 new Vue({
   router,
