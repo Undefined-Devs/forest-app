f1430420d7ca48a0fe97cd855a075d712b66f8f7
ManuelGil <atencion.ms@outlook.com>
2020-08-30 01:12:29 -0500

    Update Register View

--- src/views/NewUser.vue
+++ src/views/NewUser.vue
@@ -3,18 +3,47 @@
   <div id="content-wrapper" class="d-flex flex-column">
     <!-- Main Content -->
     <div id="content">
-
       <!-- Begin Page Content -->
       <div class="container-fluid mt-4">
-        <h1>Nuevo Usuario</h1>
+        <div class="row">
+          <div class="col-md-12">
+            <h1>Registro</h1>
+            <form role="form">
+              <div class="form-group">
+                <label for="username"> Usuario </label>
+                <input type="text" class="form-control" id="username" />
+              </div>
+              <div class="form-group">
+                <label for="email"> Correo </label>
+                <input type="email" class="form-control" id="email" />
+              </div>
+              <div class="form-group">
+                <label for="password"> Contraseña </label>
+                <input type="password" class="form-control" id="password" />
+              </div>
+              <div class="form-group">
+                <label for="fist_name"> Nombres </label>
+                <input type="text" class="form-control" id="fist_name" />
+              </div>
+              <div class="form-group">
+                <label for="last_name"> Apellidos </label>
+                <input type="text" class="form-control" id="last_name" />
+              </div>
+              <div class="form-group">
+                <label for="age"> Edad </label>
+                <input type="number" min="1" max="99" class="form-control" id="age" />
+              </div>
+              <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+          </div>
+        </div>
       </div>
       <!-- /.container-fluid -->
     </div>
     <!-- End of Main Content -->
 
     <!-- Footer -->
-    <vue-footer/>
-
+    <vue-footer />
   </div>
   <!-- End of Content Wrapper -->
 </template>
@@ -22,6 +51,6 @@
 <script>
 // @ is an alias to /src
 export default {
-  name: 'NewUser',
+  name: "NewUser",
 };
 </script>
