9baf04107ed5c86b10fcee409062fbef57033fff
Cristopher <cristopher.p.s.cto@gmail.com>
2020-08-30 00:26:19 -0500

    se agrega not found

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,6 +1,12 @@
 <template>
-  <div>
-    sdfsdfssdf
+  <div class="container-fluid">
+    <div class="alert alert-warning text-center mt-5" role="alert">
+      <h1>NOT FOUND</h1>
+      <i class="fas fa-info-circle"></i>
+      La página a la que deseas acceder no fue encontrada
+      <br>
+      <a href="/">Ir a inicio</a>
+    </div>
   </div>
 </template>
 
