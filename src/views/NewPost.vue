511308bfbc564d66c2bcea25e041bddedba15db5
Sebastian <sebastian.noguera30@gmail.com>
2020-08-30 03:37:17 -0400

    se agrega formulario para comprobar reto

--- src/views/NewPost.vue
+++ src/views/NewPost.vue
@@ -7,32 +7,21 @@
       <div class="container-fluid mt-4">
         <div class="row">
           <div class="col-md-12">
-            <h1>Registro</h1>
+            <h1>Reto aceptado</h1>
             <form role="form">
               <div class="form-group">
-                <label for="username"> Usuario </label>
-                <input type="text" class="form-control" id="username" />
+                <label for="titulo">Titulo</label>
+                <input type="text" class="form-control" id="title" />
               </div>
               <div class="form-group">
-                <label for="email"> Correo </label>
-                <input type="email" class="form-control" id="email" />
+                <label for="descripcion">Descripcion</label>
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
               </div>
               <div class="form-group">
-                <label for="password"> Contraseña </label>
-                <input type="password" class="form-control" id="password" />
+                <label for="descripcion">Subir video</label>
+                <input class="ml-4" for="video" type="file" id="video" accept="video/mp4" />
               </div>
-              <div class="form-group">
-                <label for="fist_name"> Nombres </label>
-                <input type="text" class="form-control" id="fist_name" />
-              </div>
-              <div class="form-group">
-                <label for="last_name"> Apellidos </label>
-                <input type="text" class="form-control" id="last_name" />
-              </div>
-              <div class="form-group">
-                <label for="age"> Edad </label>
-                <input type="number" min="1" max="99" class="form-control" id="age" />
-              </div>
+
               <button type="submit" class="btn btn-primary">Submit</button>
             </form>
           </div>
