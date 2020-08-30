f665919d1590811382c9b93f6b799c7e03727116
Cristopher <cristopher.p.s.cto@gmail.com>
2020-08-30 06:36:10 -0500

    se mejora vista de retos

--- src/views/Challenges.vue
+++ src/views/Challenges.vue
@@ -4,60 +4,125 @@
     <!-- Main Content -->
     <div id="content">
       <!-- Begin Page Content -->
-      <div class="container-fluid mt-4">
-        <!-- <article>
-          <p
-            class="text-dark challenge-description text-center"
-          >Realiza una maceta con 3 botellas de plastico y envia una foto o graba un video del proceso que hiciste</p>
-          <button class="button">Aceptar reto</button>
-        </article>-->
-        <div class="grid-cards">
-          <div class="card" style="width: 18rem;">
-            <div class="card-body">
-              <h5 class="card-title">Haz una maceta</h5>
-              <p
-                class="card-text"
-              >Realiza una maceta con 3 botellas de plastico y envia una foto o graba un video del proceso que hiciste</p>
-              <button class="button">Aceptar reto</button>
+      <div class="container mt-4">
+
+        
+        <div class="row" style="background-image: url('../assets/banner.jpeg');background-color: #cccccc;">
+          <div>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          </div>
+        </div>
+        
+        <div class="row">
+          <div class="col-xl-4 mb-4">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Haz una maceta</h5>
+                <p
+                  class="card-text"
+                >Realiza una maceta con 3 botellas de plastico y envia una foto o graba un video del proceso que hiciste</p>
+                <button class="btn btn-primary">Aceptar reto</button>
+              </div>
             </div>
           </div>
-          <div class="card" style="width: 18rem;">
-            <div class="card-body">
-              <h5 class="card-title">Haz una maceta</h5>
-              <p
-                class="card-text"
-              >Realiza una maceta con 3 botellas de plastico y envia una foto o graba un video del proceso que hiciste</p>
-              <button class="button">Aceptar reto</button>
+
+          <div class="col-xl-4 mb-4">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Haz una maceta</h5>
+                <p
+                  class="card-text"
+                >Realiza una maceta con 3 botellas de plastico y envia una foto o graba un video del proceso que hiciste</p>
+                <button class="btn btn-primary">Aceptar reto</button>
+              </div>
             </div>
           </div>
-          <div class="card" style="width: 18rem;">
-            <div class="card-body">
-              <h5 class="card-title">Haz una maceta</h5>
-              <p
-                class="card-text"
-              >Realiza una maceta con 3 botellas de plastico y envia una foto o graba un video del proceso que hiciste</p>
-              <button class="button">Aceptar reto</button>
+
+          <div class="col-xl-4 mb-4">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Haz una maceta</h5>
+                <p
+                  class="card-text"
+                >Realiza una maceta con 3 botellas de plastico y envia una foto o graba un video del proceso que hiciste</p>
+                <button class="btn btn-primary">Aceptar reto</button>
+              </div>
             </div>
           </div>
-          <div class="card" style="width: 18rem;">
-            <div class="card-body">
-              <h5 class="card-title">Haz una maceta</h5>
-              <p
-                class="card-text"
-              >Realiza una maceta con 3 botellas de plastico y envia una foto o graba un video del proceso que hiciste</p>
-              <button class="button">Aceptar reto</button>
+
+          <div class="col-xl-4 mb-4">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Haz una maceta</h5>
+                <p
+                  class="card-text"
+                >Realiza una maceta con 3 botellas de plastico y envia una foto o graba un video del proceso que hiciste</p>
+                <button class="btn btn-primary">Aceptar reto</button>
+              </div>
             </div>
           </div>
-          <div class="card" style="width: 18rem;">
-            <div class="card-body">
-              <h5 class="card-title">Haz una maceta</h5>
-              <p
-                class="card-text"
-              >Realiza una maceta con 3 botellas de plastico y envia una foto o graba un video del proceso que hiciste</p>
-              <button class="button">Aceptar reto</button>
+
+          <div class="col-xl-4 mb-4">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Haz una maceta</h5>
+                <p
+                  class="card-text"
+                >Realiza una maceta con 3 botellas de plastico y envia una foto o graba un video del proceso que hiciste</p>
+                <button class="btn btn-primary">Aceptar reto</button>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-xl-4 mb-4">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Haz una maceta</h5>
+                <p
+                  class="card-text"
+                >Realiza una maceta con 3 botellas de plastico y envia una foto o graba un video del proceso que hiciste</p>
+                <button class="btn btn-primary">Aceptar reto</button>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-xl-4 mb-4">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Haz una maceta</h5>
+                <p
+                  class="card-text"
+                >Realiza una maceta con 3 botellas de plastico y envia una foto o graba un video del proceso que hiciste</p>
+                <button class="btn btn-primary">Aceptar reto</button>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-xl-4 mb-4">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Haz una maceta</h5>
+                <p
+                  class="card-text"
+                >Realiza una maceta con 3 botellas de plastico y envia una foto o graba un video del proceso que hiciste</p>
+                <button class="btn btn-primary">Aceptar reto</button>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-xl-4 mb-4">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Haz una maceta</h5>
+                <p
+                  class="card-text"
+                >Realiza una maceta con 3 botellas de plastico y envia una foto o graba un video del proceso que hiciste</p>
+                <button class="btn btn-primary">Aceptar reto</button>
+              </div>
             </div>
           </div>
         </div>
+
       </div>
       <!-- /.container-fluid -->
     </div>
