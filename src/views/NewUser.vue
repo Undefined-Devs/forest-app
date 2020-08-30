22a97c3b7eed8962ffd168999a8c5d35626fc450
ManuelGil <atencion.ms@outlook.com>
2020-08-30 02:07:07 -0500

    Update Register View

--- src/views/NewUser.vue
+++ src/views/NewUser.vue
@@ -8,10 +8,10 @@
         <div class="row">
           <div class="col-md-12">
             <h1>Registro</h1>
-            <form role="form">
+            <form role="form" @submit="register()">
               <div class="form-group">
                 <label for="username"> Usuario </label>
-                <input type="text" class="form-control" id="username" v-model="nickname" />
+                <input type="text" class="form-control" id="username" v-model="this.nickname" />
               </div>
               <div class="form-group">
                 <label for="email"> Correo </label>
@@ -20,7 +20,7 @@
                   class="form-control"
                   id="email"
                   pattern="^[a-zA-Z0-9][a-zA-Z0-9._%+-]+@[a-zA-Z0-9-]+\.[a-zA-Z]{2,3}(?:\.[a-zA-Z]{2})?$"
-                  v-model="email"
+                  v-model="this.email"
                 />
               </div>
               <div class="form-group">
@@ -29,21 +29,27 @@
                   type="password"
                   class="form-control"
                   id="password"
-                  pattern="(?=^.{6,}$)(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).*$"
-                  v-model="password"
+                  v-model="this.password"
                 />
               </div>
               <div class="form-group">
                 <label for="fistname"> Nombres </label>
-                <input type="text" class="form-control" id="fistname" v-model="name" />
+                <input type="text" class="form-control" id="fistname" v-model="this.name" />
               </div>
               <div class="form-group">
                 <label for="lastname"> Apellidos </label>
-                <input type="text" class="form-control" id="lastname" v-model="last_name" />
+                <input type="text" class="form-control" id="lastname" v-model="this.last_name" />
               </div>
               <div class="form-group">
                 <label for="age"> Edad </label>
-                <input type="number" min="1" max="99" class="form-control" id="age" v-model="age" />
+                <input
+                  type="number"
+                  min="1"
+                  max="99"
+                  class="form-control"
+                  id="age"
+                  v-model="this.age"
+                />
               </div>
               <button type="submit" class="btn btn-primary">Enviar</button>
             </form>
