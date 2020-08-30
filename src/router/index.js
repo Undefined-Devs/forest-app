fa007e02c22a1aa72d7f114aae60942982b51a6e
Cristopher <cristopher.p.s.cto@gmail.com>
2020-08-29 23:07:49 -0500

    se agrega vista para no encontrados

--- src/router/index.js
+++ src/router/index.js
@@ -18,6 +18,14 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
   },
+  {
+    path: '*',
+    name: 'NotFound',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/NotFound.vue'),
+  },
 ];
 
 const router = new VueRouter({
---
+++ src/views/NotFound.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    sdfsdfssdf
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+export default {
+  name: 'NotFound',
+};
+</script>
