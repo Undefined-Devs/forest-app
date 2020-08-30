b8bcec1d813732a9eaae57fde5b12827a0052eb1
Cristopher <cristopher.p.s.cto@gmail.com>
2020-08-29 23:08:07 -0500

    se agrega pantalla de inicio

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,158 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- Content Wrapper -->
+  <div id="content-wrapper" class="d-flex flex-column">
+    <!-- Main Content -->
+    <div id="content">
+
+      <!-- Begin Page Content -->
+      <div class="container-fluid mt-4">
+
+        <top/>
+
+        <hr>
+
+        <!-- Content Row -->
+
+        <div class="row">
+          <!-- Area Chart -->
+          <div class="col-xl-12">
+            <div class="card shadow mb-4">
+              <!-- Card Header - Dropdown -->
+              <div
+                class="card-header py-3 d-flex flex-row
+                align-items-center justify-content-between"
+              >
+                <div class="h5 mb-0 text-gray-800">
+                  <strong>Cristopher PS </strong>
+                  <label class="font-weight-bold text-primary text-uppercase mb-1">
+                    +40k
+                  </label>
+                </div>
+                <a href="#" class="badge badge-success">Reto: Planta 3 arboles en tu comunidad</a>
+                <a href="#" class="m-0 font-weight-bold text-primary">Ver más</a>
+              </div>
+              <!-- Card Body -->
+              <div class="card-body">
+                <div class="chart-area">
+                  <video id="player" controls width="100%" height="100%">
+                    <source src="https://sitevurokrazia.s3.amazonaws.com/uzn5ox97o7eqkdzbh6o09h7snbv3?response-content-disposition=attachment%3B%20filename%3D%22video.mp4%22%3B%20filename%2A%3DUTF-8%27%27video.mp4&response-content-type=video%2Fmp4&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA3FWQSHSPBCXLT3HK%2F20200830%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20200830T003444Z&X-Amz-Expires=86400&X-Amz-SignedHeaders=host&X-Amz-Signature=17733d81f7753542145daab0d7a073bd5209c0b1eb071045ce7754440b31b09c" type="video/mp4">
+                  </video>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="row">
+          <!-- Area Chart -->
+          <div class="col-xl-12">
+            <div class="card shadow mb-4">
+              <!-- Card Header - Dropdown -->
+              <div
+                class="card-header py-3 d-flex flex-row
+                align-items-center justify-content-between"
+              >
+                <div class="h5 mb-0 text-gray-800">
+                  <strong>Cristopher PS </strong>
+                  <label class="font-weight-bold text-primary text-uppercase mb-1">
+                    +40k
+                  </label>
+                </div>
+                <a href="#" class="badge badge-success">Reto: Planta 3 arboles en tu comunidad</a>
+                <a href="#" class="m-0 font-weight-bold text-primary">Ver más</a>
+              </div>
+              <!-- Card Body -->
+              <div class="card-body">
+                <div class="chart-area">
+                  <video id="player" controls width="100%" height="100%">
+                    <source src="https://sitevurokrazia.s3.amazonaws.com/uzn5ox97o7eqkdzbh6o09h7snbv3?response-content-disposition=attachment%3B%20filename%3D%22video.mp4%22%3B%20filename%2A%3DUTF-8%27%27video.mp4&response-content-type=video%2Fmp4&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA3FWQSHSPBCXLT3HK%2F20200830%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20200830T003444Z&X-Amz-Expires=86400&X-Amz-SignedHeaders=host&X-Amz-Signature=17733d81f7753542145daab0d7a073bd5209c0b1eb071045ce7754440b31b09c" type="video/mp4">
+                  </video>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="row">
+          <!-- Area Chart -->
+          <div class="col-xl-12">
+            <div class="card shadow mb-4">
+              <!-- Card Header - Dropdown -->
+              <div
+                class="card-header py-3 d-flex flex-row
+                align-items-center justify-content-between"
+              >
+                <div class="h5 mb-0 text-gray-800">
+                  <strong>Cristopher PS </strong>
+                  <label class="font-weight-bold text-primary text-uppercase mb-1">
+                    +40k
+                  </label>
+                </div>
+                <a href="#" class="badge badge-success">Reto: Planta 3 arboles en tu comunidad</a>
+                <a href="#" class="m-0 font-weight-bold text-primary">Ver más</a>
+              </div>
+              <!-- Card Body -->
+              <div class="card-body">
+                <div class="chart-area">
+                  <video id="player" controls width="100%" height="100%">
+                    <source src="https://sitevurokrazia.s3.amazonaws.com/uzn5ox97o7eqkdzbh6o09h7snbv3?response-content-disposition=attachment%3B%20filename%3D%22video.mp4%22%3B%20filename%2A%3DUTF-8%27%27video.mp4&response-content-type=video%2Fmp4&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA3FWQSHSPBCXLT3HK%2F20200830%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20200830T003444Z&X-Amz-Expires=86400&X-Amz-SignedHeaders=host&X-Amz-Signature=17733d81f7753542145daab0d7a073bd5209c0b1eb071045ce7754440b31b09c" type="video/mp4">
+                  </video>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="row">
+          <!-- Area Chart -->
+          <div class="col-xl-12">
+            <div class="card shadow mb-4">
+              <!-- Card Header - Dropdown -->
+              <div
+                class="card-header py-3 d-flex flex-row
+                align-items-center justify-content-between"
+              >
+                <div class="h5 mb-0 text-gray-800">
+                  <strong>Cristopher PS </strong>
+                  <label class="font-weight-bold text-primary text-uppercase mb-1">
+                    +40k
+                  </label>
+                </div>
+                <a href="#" class="badge badge-success">Reto: Planta 3 arboles en tu comunidad</a>
+                <a href="#" class="m-0 font-weight-bold text-primary">Ver más</a>
+              </div>
+              <!-- Card Body -->
+              <div class="card-body">
+                <div class="chart-area">
+                  <video id="player" controls width="100%" height="100%">
+                    <source src="https://sitevurokrazia.s3.amazonaws.com/uzn5ox97o7eqkdzbh6o09h7snbv3?response-content-disposition=attachment%3B%20filename%3D%22video.mp4%22%3B%20filename%2A%3DUTF-8%27%27video.mp4&response-content-type=video%2Fmp4&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA3FWQSHSPBCXLT3HK%2F20200830%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20200830T003444Z&X-Amz-Expires=86400&X-Amz-SignedHeaders=host&X-Amz-Signature=17733d81f7753542145daab0d7a073bd5209c0b1eb071045ce7754440b31b09c" type="video/mp4">
+                  </video>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- /.container-fluid -->
+    </div>
+    <!-- End of Main Content -->
+
+    <!-- Footer -->
+    <footer class="sticky-footer bg-white">
+      <div class="container my-auto">
+        <div class="copyright text-center my-auto">
+          <span>Powered By #UndefindedDevs 2020</span>
+        </div>
+      </div>
+    </footer>
+    <!-- End of Footer -->
   </div>
+  <!-- End of Content Wrapper -->
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld,
-  },
 };
 </script>
