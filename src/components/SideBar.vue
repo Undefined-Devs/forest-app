304bfe061c60e1b1aa606bb32db2cf2968865fc8
Cristopher <cristopher.p.s.cto@gmail.com>
2020-08-29 23:06:51 -0500

    se crea componente de sidebar

---
+++ src/components/SideBar.vue
@@ -0,0 +1,65 @@
+<template>
+  <!-- Sidebar -->
+  <ul
+    class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion"
+    id="accordionSidebar"
+  >
+    <!-- Sidebar - Brand -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+      <div class="sidebar-brand-text mx-3">Forest App <sup>1.0</sup></div>
+    </a>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider my-0" />
+
+    <li class="nav-item">
+      <a
+        class="nav-link text-center"
+        href="#"
+      >
+        <span class="mr-2 d-none d-lg-inline small"><strong>Cristopher PS</strong></span>
+        <img
+          class="img-profile rounded-circle"
+          src="https://source.unsplash.com/QAB-WJcbgJk/60x60"
+        />
+      </a>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider" />
+
+    <!-- Heading -->
+    <div class="sidebar-heading">
+      MENU
+    </div>
+
+    <li class="nav-item active">
+      <a class="nav-link" href="/">
+        <i class="fas fa-fw fa-tachometer-alt"></i>
+        <span>Inicio</span></a
+      >
+    </li>
+
+    <li class="nav-item">
+      <a class="nav-link" href="/challenges">
+        <i class="fas fa-fw fa-tachometer-alt"></i>
+        <span>Retos</span></a
+      >
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider d-none d-md-block" />
+
+  </ul>
+  <!-- End of Sidebar -->
+</template>
+
+<script>
+export default {
+  name: 'SideBar',
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
